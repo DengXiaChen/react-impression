2065b15b594331310a2406dbc477465dd5e53818
DengXiaChen <1711327128@qq.com>
2017-06-14 18:38:04 +0800

    [ADD] jest + enzyme

---
+++ src/scripts/test/component/Alert.spec.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Alert } from '../../components/impression';
+
+const setup = () => {
+    const props = {
+        children: 'alert test',
+        type: 'danger',
+        onClick: jest.fn(),
+        btnText: '确认按钮',
+    };
+
+    const wrapper = shallow(<Alert {...props} />);
+
+    return {
+        props,
+        wrapper,
+    };
+};
+
+describe('Todo', () => {
+    const { props, wrapper } = setup();
+
+    it('render', () => {
+        expect(wrapper.find('.alert').exists()).toBe(true);
+    });
+
+    it('type', () => {
+        expect(wrapper.find('i.text-danger').exists()).toBe(true);
+    });
+
+    it('btnText', () => {
+        expect(wrapper.find('.alert-footer').children().text()).toBe('确认按钮');
+    });
+
+    it('children', () => {
+        expect(wrapper.find('.alert-body').children().text()).toBe('alert test');
+    });
+
+    it('click', () => {
+        wrapper.find('.alert-footer').simulate('click');
+        expect(props.onClick).toHaveBeenCalled();
+    });
+});
---
+++ src/scripts/test/component/Button.spec.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Button } from '../../components/impression';
+
+const setup = () => {
+    const props = {
+        theme: 'primary',
+        onClick: jest.fn(),
+        outline: false,
+        size: 'lg',
+        shape: 'pill',
+        block: true,
+        disabled: true,
+    };
+
+    const wrapper = shallow(<Button {...props} />);
+
+    return {
+        props,
+        wrapper,
+    };
+};
+
+describe('Todo', () => {
+    const { props, wrapper } = setup();
+
+    // 通过 input 是否存在来判断 Todo组件是否被渲染
+    it('render', () => {
+        expect(wrapper.find('button').exists()).toBe(true);
+    });
+
+    // 当点击 单选按钮，onClick 方法应该被调用
+    it('click', () => {
+        wrapper.find('button').simulate('click');
+        expect(props.onClick).toHaveBeenCalled();
+    });
+
+    it('theme', () => {
+        expect(wrapper.find('.btn-primary').exists()).toBe(true);
+    });
+
+    it('size', () => {
+        expect(wrapper.find('.btn-lg').exists()).toBe(true);
+    });
+
+    it('shape', () => {
+        expect(wrapper.find('.btn-pill').exists()).toBe(true);
+    });
+});
